b67c5a4ac6abca947d192cf53c6e13e806701307
Levanbuchukuri1993@gmail.com <Levanbuchukuri1993@gmail.com>
2019-12-09 01:46:08 +0600

    init

---
+++ TwitchViewersNotifier/TwitchData.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+
+namespace TwitchViewersNotifier
+{
+    class TwitchData
+    {
+        public static String getClientId()
+        {
+            try
+            {
+                String jsonString = File.ReadAllText("config.json");
+                String client_id = "Not found shit";
+                Dictionary<string, string> data = JsonConvert.DeserializeObject<Dictionary<string, string>>(jsonString);
+
+                data.TryGetValue("client_id", out client_id);
+                return client_id;
+            } catch (Exception ex)
+            {
+                return ex.ToString();
+            }
+        }
+    }
+}

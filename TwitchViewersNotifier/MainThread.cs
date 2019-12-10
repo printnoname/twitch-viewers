97faa57365c4d4388deab56b3473238269ec5574
Levanbuchukuri1993@gmail.com <Levanbuchukuri1993@gmail.com>
2019-12-11 00:03:10 +0600

    adding async stuff

---
+++ TwitchViewersNotifier/MainThread.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TwitchViewersNotifier
+{
+    class MainThread
+    {
+        public static void MainRoutine()
+        {
+            while (true) { 
+                Debug.Print("Thread working");
+                Thread.Sleep(5000);
+            }
+        }
+
+        static async Task UserIdCall(String username)
+        {
+            HttpClient client = new HttpClient();
+            return client.GetAsync("http://webcode.me");
+        }
+    }
+}
--- TwitchViewersNotifier/MainWindow.xaml.cs
+++ TwitchViewersNotifier/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 
@@ -9,7 +10,7 @@ namespace TwitchViewersNotifier
 {
     public partial class MainWindow : Window
     {
-
+        private 
         enum State
         {
             Online,
@@ -25,6 +26,11 @@ namespace TwitchViewersNotifier
         public MainWindow()
         {
             InitializeComponent();
+            StateMachine<State, Trigger> appState = initStateMachine();
+
+            Thread mainThread = new Thread(MainThread.MainRoutine);
+            mainThread.Start();
+
         }
 
         private void Sync(object sender, RoutedEventArgs e)
@@ -35,8 +41,25 @@ namespace TwitchViewersNotifier
         private StateMachine<State,Trigger> initStateMachine()
         {
             StateMachine<State, Trigger> stateMachine = new StateMachine<State, Trigger>(State.Offline);
+            return stateMachine;
+            //var phoneCall = new StateMachine<State, Trigger>(State.OffHook);
 
-            
+            //phoneCall.Configure(State.OffHook)
+            //    .Permit(Trigger.CallDialled, State.Ringing);
+
+            //phoneCall.Configure(State.Ringing)
+            //    .Permit(Trigger.CallConnected, State.Connected);
+
+            //phoneCall.Configure(State.Connected)
+            //    .OnEntry(() => StartCallTimer())
+            //    .OnExit(() => StopCallTimer())
+            //    .Permit(Trigger.LeftMessage, State.OffHook)
+            //    .Permit(Trigger.PlacedOnHold, State.OnHold);
+
+            //// ...
+
+            //phoneCall.Fire(Trigger.CallDialled);
+            //Assert.AreEqual(State.Ringing, phoneCall.State);
         }
 
         private void UnSync(object sender, RoutedEventArgs e)

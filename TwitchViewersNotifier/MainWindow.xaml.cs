438b919a521d1f8f0507478aa8a7180c797c113a
Levanbuchukuri1993@gmail.com <Levanbuchukuri1993@gmail.com>
2019-12-12 00:19:31 +0600

    added first request

--- TwitchViewersNotifier/MainWindow.xaml.cs
+++ TwitchViewersNotifier/MainWindow.xaml.cs
@@ -1,7 +1,11 @@
-﻿using Stateless;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Stateless;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
@@ -10,66 +14,102 @@ namespace TwitchViewersNotifier
 {
     public partial class MainWindow : Window
     {
-        private 
+        private StateMachine<State, Trigger> appState;
+        private String clientId;
+
         enum State
         {
             Online,
-            Offline
+            Offline,
+            //ErrorGetClientId,
+            //ErrorInternetConnection,
+            //ErrorGetStreamInfo
         }
 
         enum Trigger
         {
-            Sync,
-            Unsync
+            SyncButtonClicked,
+            UnsyncButtonClicked
         }
 
         public MainWindow()
         {
             InitializeComponent();
-            StateMachine<State, Trigger> appState = initStateMachine();
+            clientId = TwitchData.getClientId();
+            appState = initStateMachine();
+        }
 
-            Thread mainThread = new Thread(MainThread.MainRoutine);
-            mainThread.Start();
+        private async void OnlineRoutine()
+        {
+            HttpClient client = new HttpClient();
+            await Task.Run(async () =>
+            {
+                String userIdRequestUrl = "https://api.twitch.tv/helix/users?login=printnoname";
+
+                HttpClient httpClient = new HttpClient();
+                HttpRequestMessage requestMessage = new HttpRequestMessage(HttpMethod.Get, userIdRequestUrl);
+                HttpResponseMessage responseMessage = new HttpResponseMessage();
+                HttpContent httpContent;
+
+                while (appState.IsInState(State.Online))
+            {
+                    
+                    try
+                    {
+                        requestMessage.Headers.Add("Client-ID", clientId);
+                        responseMessage = await httpClient.SendAsync(requestMessage);
+                        httpContent = responseMessage.Content;
+                        string result = await httpContent.ReadAsStringAsync();
+
+                        JObject json = JObject.Parse(result);
+                        String userId = JObject.Parse(json.GetValue("data")[0].ToString()).GetValue("id").ToString();
+
+                        if(userId.Equals(""))
+                        {
+                            throw new Exception();
+                        }
+
+                    } catch (Exception ex) {
+                       
+                    } finally {
+                        ((IDisposable)httpClient).Dispose();
+                        ((IDisposable)requestMessage).Dispose();
+                        ((IDisposable)responseMessage).Dispose();
+                    }
+                Thread.Sleep(5000);
+                }
+
+            });
 
         }
 
         private void Sync(object sender, RoutedEventArgs e)
         {
-            Debug.Print(TwitchData.getClientId());
+            appState.FireAsync(Trigger.SyncButtonClicked);
+            syncButton.Visibility = Visibility.Hidden;
+            unsyncButton.Visibility = Visibility.Visible;
         }
 
         private StateMachine<State,Trigger> initStateMachine()
         {
             StateMachine<State, Trigger> stateMachine = new StateMachine<State, Trigger>(State.Offline);
+
+            stateMachine.Configure(State.Offline)
+                .OnEntry(()=> Debug.Print("Utility is offline"))
+                .Permit(Trigger.SyncButtonClicked, State.Online);
+
+            stateMachine.Configure(State.Online)
+                .OnEntryAsync(async () => await Task.Run(()=>OnlineRoutine()))
+                .Permit(Trigger.UnsyncButtonClicked, State.Offline);
+
             return stateMachine;
-            //var phoneCall = new StateMachine<State, Trigger>(State.OffHook);
-
-            //phoneCall.Configure(State.OffHook)
-            //    .Permit(Trigger.CallDialled, State.Ringing);
-
-            //phoneCall.Configure(State.Ringing)
-            //    .Permit(Trigger.CallConnected, State.Connected);
-
-            //phoneCall.Configure(State.Connected)
-            //    .OnEntry(() => StartCallTimer())
-            //    .OnExit(() => StopCallTimer())
-            //    .Permit(Trigger.LeftMessage, State.OffHook)
-            //    .Permit(Trigger.PlacedOnHold, State.OnHold);
-
-            //// ...
-
-            //phoneCall.Fire(Trigger.CallDialled);
-            //Assert.AreEqual(State.Ringing, phoneCall.State);
         }
 
         private void UnSync(object sender, RoutedEventArgs e)
         {
-
-        }
-
-        public async Task MainLoop()
-        {
-
+            appState.Fire(Trigger.UnsyncButtonClicked);
+            syncButton.Visibility = Visibility.Visible;
+            unsyncButton.Visibility = Visibility.Hidden;
         }
     }
 }
